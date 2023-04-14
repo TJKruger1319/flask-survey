1db42f635970217149182549ab62a8218c7d9baf
TJ <TJKruger1319@gmail.com>
2023-04-13 21:45:39 -0400

    Makes the HTML files and the starting page

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, redirect, request
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+@app.route("/")
+def start_page():
+    return render_template("start.html", title=satisfaction_survey.title,
+                            instruct=satisfaction_survey.instructions)
