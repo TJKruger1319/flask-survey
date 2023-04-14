72b303898878c8cd88a270ece5d1a026e3977ecf
TJ <TJKruger1319@gmail.com>
2023-04-13 21:25:47 -0400

    intial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+debug = DebugToolbarExtension(app)
