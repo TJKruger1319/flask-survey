cfcd4e61c19058b5e452cb63415ace9eed02090b
TJ <TJKruger1319@gmail.com>
2023-04-13 22:14:12 -0400

    adds the question page

--- app.py
+++ app.py
@@ -12,3 +12,9 @@ responses = []
 def start_page():
     return render_template("start.html", title=satisfaction_survey.title,
                             instruct=satisfaction_survey.instructions)
+
+@app.route("/questions/<num>")
+def show_question(num):
+    return render_template("question.html", quest=satisfaction_survey.questions[int(num)].question,
+                        op_one = satisfaction_survey.questions[int(num)].choices[0],
+                        op_two = satisfaction_survey.questions[int(num)].choices[1])
