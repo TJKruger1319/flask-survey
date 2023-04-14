2520c466a44988f346040288c3049f5d2ad42d04
TJ <TJKruger1319@gmail.com>
2023-04-13 23:36:54 -0400

    Makes the question page

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 responses = []
+
 @app.route("/")
 def start_page():
     return render_template("start.html", title=satisfaction_survey.title,
                             instruct=satisfaction_survey.instructions)
 
-@app.route("/questions/<num>")
+@app.route("/questions/<int:num>")
 def show_question(num):
+    session["nums"] = num
     return render_template("question.html", quest=satisfaction_survey.questions[int(num)].question,
                         op_one = satisfaction_survey.questions[int(num)].choices[0],
-                        op_two = satisfaction_survey.questions[int(num)].choices[1])
+                        op_two = satisfaction_survey.questions[int(num)].choices[1], num=num)
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    answer = request.form["option"]
+    num = session["nums"]
+    num = num+1
+    responses.append(answer) 
+    return redirect(f"/questions/{num}")
